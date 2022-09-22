d41b6d9ab94f577bae94c869c6020c7220019a39
AOKaizoMac <kolbywolf@gmail.com>
2022-09-21 21:26:45 -0400

    End of Night Version

--- db/index.js
+++ db/index.js
@@ -2,17 +2,31 @@ const Sequelize = require("sequelize");
 const db = new Sequelize("postgres://localhost/bookmarker");
 
 const Bookmark = db.define("Bookmark", {
-  id: { type: Sequelize.INTEGER, allowNull: false },
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
   name: { type: Sequelize.STRING, allowNull: false },
   url: { type: Sequelize.STRING, allowNull: false },
   categoryId: { type: Sequelize.STRING, allowNull: false },
 });
 
 const Category = db.define("Category", {
-  id: { type: Sequelize.INTEGER, allowNull: false },
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
   name: { type: Sequelize.STRING, allowNull: false },
 });
 
-db.sync()
+async function dbServer() {
+  await db.sync();
+}
 
-module.exports({Bookmark, Category})
+dbServer();
+
+module.exports({ Bookmark, Category });
--- seed.js
+++ seed.js
@@ -1,3 +1,26 @@
 const Sequelize = require("sequelize");
-const tables = require('.db')
+const tables = require('./db')
+const {Bookmark, Category} = tables
+
+
+function createBookmark(newName, newUrl, newCategory) {
+    Bookmark.create({name: newName,
+        url: newUrl,
+        category: newCategory})
+}
+function createCategory(newCategory) {
+    Category.create({
+        name: newCategory})
+}
+
+createCategory('coding')
+createCategory('search')
+createCategory('jobs')
+
+createBookmark('Google', 'https://www.google.com/', 'search')
+createBookmark('Stack Overflow', 'https://stackoverflow.com/', 'code')
+createBookmark('Bing', 'https://www.bing.com/', 'search')
+createBookmark('Linkedin', 'https://www.linkedin.com/', 'jobs')
+createBookmark('Indeed', 'https://www.indeed.com/', 'jobs')
+createBookmark('MDN', 'https://www.developer.mozilla.org/en-US/', 'code')
 
